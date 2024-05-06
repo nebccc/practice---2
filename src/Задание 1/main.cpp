7ce4893276a616556ba33de986c17555a7ef4718
nebccc <nebccccc@gmail.com>
2024-05-07 05:35:13 +0700

    1 and 5 th

--- src/Задание 1/main.cpp
+++ src/Задание 1/main.cpp
@@ -1,59 +1,26 @@
 #include <iostream>
-#include <cmath>
 #include <tuple>
 
 using namespace std;
-
-tuple<int64_t, int64_t, int64_t> init();
-
-bool isPrime(int64_t n);
 
 int64_t f(int64_t x, int64_t y, int64_t n, int64_t r);
 int64_t powMod(int64_t x, int64_t y, int64_t n);
 
 int main() {
     int64_t n, y, p;
-    tie(n, y, p) = init();
+    cin >> n >> y >> p;
 
     // Использование теоремы Ферма для проверки
     if (powMod(n, p-1, p) != 1) {
-        cout << "Ошибка: теорема Ферма не выполняется для данных n и p." << endl;
-    } else {
-        cout << powMod(n, y, p) << endl;
+        cout << "Теорема Ферма не выполняется для данных n и p." << endl;
+        cout << "a^x mod p = " << powMod(n, y, p);
+    }
+
+    else{
+        cout << "Теорема Ферма выполняется для данных n и p = " << powMod(n, y, p - 1);
     }
 
     return 0;
-}
-
-tuple<int64_t, int64_t, int64_t> init() {
-    int64_t x, y, p;
-
-    while (true) {
-        cout << "Введите любое натуральное число n: ";
-        cin >> x;
-
-        cout << "Введите любое натуральное число y (степень): ";
-        cin >> y;
-
-        cout << "Введите любое простое число p: ";
-        cin >> p;
-
-        if (isPrime(p) && x % p != 0) break;
-        else cout << "Пожалуйста, убедитесь, что p простое и n не делится на p." << endl;
-    }
-
-    return make_tuple(x, y, p);
-}
-
-bool isPrime(int64_t n) {
-    if (n <= 1) return false;
-
-    int64_t limit = sqrt(n);
-    for (int64_t i = 2; i <= limit; i++) {
-        if (n % i == 0) return false;
-    }
-
-    return true;
 }
 
 int64_t f(int64_t x, int64_t y, int64_t n, int64_t r) {
